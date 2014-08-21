5be4eb1050f94b97889f0680274c57f9827129e6
Nicolas Di Vara <nikolino77@mbp-di-nicolas.station>
2014-08-21 14:18:34 +0200

    add macro

--- extract_sigma_tree.C
+++ extract_sigma_tree.C
@@ -31,7 +31,7 @@ using namespace std;
 
 int main()
 {
-
+    
     TFile* hfile = new TFile("/media/Elements/temp/simu_0_10000.root","OPEN");
     TFile* hfile_out = new TFile("/media/Elements/temp/simu_0_10000_out.root","RECREATE");
     
@@ -48,10 +48,10 @@ int main()
     
     vector<double> *shao_stamps = new vector<double>();
     Singles->SetBranchAddress("shao_stamps",&shao_stamps);
-     
+    
     vector<double >* shao_smear_stamps = new vector<double>();
     Singles->SetBranchAddress("shao_smear_stamps",&shao_smear_stamps);
-
+    
     vector<double >* conv_stamps = new vector<double>();
     Singles->SetBranchAddress("conv_stamps",&conv_stamps);
     
@@ -61,62 +61,90 @@ int main()
     vector<double >* sum_smear_stamps = new vector<double>();
     Singles->SetBranchAddress("sum_smear_stamps",&sum_smear_stamps);
     
-    Int_t nentries = Singles->GetEntries(); 
-    cout << "Num entries = " << nentries << endl;	
+    Int_t nentries = Singles->GetEntries();
+    cout << "Num entries = " << nentries << endl;
     
     double min = 0.;
     double max = 1000e-9;
     int nbins = 1000000;
-    int order = 100;
+    int order = 0;
     
-    TTree *tree_sigma = new TTree("tree_sigma", "extract_sigma");
+    TTree *tree_out = new TTree("tree_out", "simu_crao_out");
     
-    TH1D* n_photon_shao_ex = new TH1D("n_photon_shao_ex","n_photon_shao_ex",nbins, min, max);
-    tree_sigma -> Branch("n_photon_shao_ex",&n_photon_shao_ex);
+    vector<TH1D* >* n_photon_shao		= new vector<TH1D* >();
+    tree -> Branch("n_photon_shao",n_photon_shao);
     
-    TH1D* n_photon_shao_smear_ex = new TH1D("n_photon_shao_ex","n_photon_shao_smear_ex",nbins, min, max);
-    tree_sigma -> Branch("n_photon_shao_smear_ex",&n_photon_shao_smear_ex);
     
-    TH1D* n_photon_conv_ex = new TH1D("n_photon_conv_ex","n_photon_conv_ex",nbins, min, max);
-    tree_sigma -> Branch("n_photon_conv_ex",&n_photon_conv_ex);
-    
-    TH1D* n_photon_sum_ex = new TH1D("n_photon_sum_ex","n_photon_sum_ex",nbins, min, max);
-    tree_sigma -> Branch("n_photon_sum_ex",&n_photon_sum_ex);
-    
-    TH1D* n_photon_sum_smear_ex = new TH1D("n_photon_sum_smear_ex","n_photon_sum_smear_ex",nbins, min, max);
-    tree_sigma -> Branch("n_photon_sum_smear_ex",&n_photon_sum_smear_ex);
+    // vector<TH1D* >* n_photon_shao		= new vector<TH1D* >();
+    //   vector<TH1D* >* n_photon_shao_smear		= new vector<TH1D* >();
+    //  vector<TH1D* >* n_photon_conv		= new vector<TH1D* >();
+    //  vector<TH1D* >* n_photon_sum		= new vector<TH1D* >();
+    //  vector<TH1D* >* n_photon_sum_smear		= new vector<TH1D* >();
     
     for(int j = 0; j < order; j++)
     {
-            std::cout << "Evento " << j << std::endl;
-      for(int i = 0; i < nentries; i++)
-      {
-        Singles->GetEntry(i);
-          cout << i << endl;
-          n_photon_shao_ex	->	Fill(shao_stamps -> at (j));
-        n_photon_shao_smear_ex->	Fill(shao_smear_stamps -> at (j));
-        n_photon_conv_ex	->	Fill(conv_stamps -> at (j));
-        n_photon_sum_ex		-> Fill(sum_stamps -> at (j));
-        n_photon_sum_smear_ex	->  Fill(sum_smear_stamps -> at (j));
-      }
-    
-      tree_sigma -> Fill();
+        stringstream ss;
+        ss << j + 1;
+        string num = ss.str();
+        string histo_name_shao 		= "shao_photon_" + num;
+        string histo_name_shao_smear 	= "shao_smear_photon_" + num;
+        string histo_name_conv 		= "conv_photon_" + num;
+        string histo_name_sum 		= "sum_photon_" + num;
+        string histo_name_sum_smear 	= "sum_smear_photon_" + num;
+        
+        n_photon_shao		-> push_back(new TH1D(histo_name_shao.c_str(),histo_name_shao.c_str(),nbins,min,max));
+        // n_photon_shao_smear	-> push_back(new TH1D(histo_name_shao_smear.c_str(),histo_name_shao_smear.c_str(),nbins,min,max));
+        // n_photon_conv		-> push_back(new TH1D(histo_name_conv.c_str(),histo_name_conv.c_str(),nbins,min,max));
+        // n_photon_sum		-> push_back(new TH1D(histo_name_sum.c_str(),histo_name_sum.c_str(),nbins,min,max));
+        // n_photon_sum_smear	-> push_back(new TH1D(histo_name_sum_smear.c_str(),histo_name_sum_smear.c_str(),nbins,min,max));
     }
-
+    
+    
+    for(int i = 0; i < 10; i++)
+    {
+        if(i%500 == 0)
+        {
+            std::cout << "Evento " << i << std::endl;
+        }
+        
+        Singles->GetEntry(i);
+        
+        for(int j = 0; j < order; j++)
+        {
+            n_photon_shao		-> at(j) -> Fill(shao_stamps -> at (j));
+            //n_photon_shao_smear	-> at(j) -> Fill(shao_smear_stamps -> at (j));
+            //n_photon_conv		-> at(j) -> Fill(conv_stamps -> at (j));
+            //n_photon_sum		-> at(j) -> Fill(sum_stamps -> at (j));
+            //n_photon_sum_smear	-> at(j) -> Fill(sum_smear_stamps -> at (j));
+        }
+    }
+    tree_out -> Fill();
+    
     hfile -> Close();
     
-    hfile_out -> cd();
-    tree_sigma -> Write();
-    shao -> Write();
-    //shao_smear -> Write();
-    //cer -> Write();
-    //cer_smear -> Write();
-    sum -> Write();
-    conv_last -> Write();
-    irf -> Write();
     
-    hfile_out -> Close();
-     
-    return 0;
+    
+    
+    hfile_out -> cd();
 
+    tree_out -> Write();
+    
+//    shao 	-> Write();
+    //shao_smear  -> Write();
+    //cer	  -> Write();
+    //cer_smear	  -> Write();
+  //  sum		-> Write();
+  //  conv_last	-> Write();
+   // irf		-> Write();
+    
+    //n_photon_shao -> at(j) -> GetRMS());
+    //n_photon_shao_smear	-> at(j) -> GetRMS());
+    //n_photon_conv-> at(j) -> GetRMS());
+    //n_photon_sum-> at(j) -> GetRMS());
+    //n_photon_sum_smear-> at(j) -> GetRMS());   
+   // hfile_out -> Write();
+    hfile_out -> Close();
+    
+    return 0;
+    
 }
